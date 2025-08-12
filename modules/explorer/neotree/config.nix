c39d8a2c65180c0e30dcd4757703bb30fedb9045
jason <jason@ridgway-taylor.co.uk>
2025-08-12 07:48:01 +0100

    fix: corrected NeotTree toggle

--- modules/explorer/neotree/config.nix
+++ modules/explorer/neotree/config.nix
@@ -23,7 +23,7 @@ vim.luaConfigRC.neotree-keys = nvim.dag.entryAnywhere ''
             { "<leader>e", group = "Explorer", icon = get_icon("default", "directory") },
             
             -- Explorer commands
-            { "<leader>E", "<cmd>NeoTree<CR>", desc = "Toggle NeoTree", icon = get_icon("default", "directory") },
+            { "<leader>E", "<cmd>Neotree toggle<CR>", desc = "Toggle NeoTree", icon = get_icon("default", "directory") },
             { "<leader>eb", "<cmd>Neotree source=buffers position=left<CR>", desc = "Buffer explorer", icon = get_icon("default", "file") },
             { "<leader>ef", "<cmd>Neotree filesystem reveal left<CR>", desc = "File explorer (reveal)", icon = get_icon("default", "directory") },
             { "<leader>eg", "<cmd>Neotree git_status<CR>", desc = "Git status explorer", icon = get_icon("filetype", "git") },
@@ -32,7 +32,7 @@ vim.luaConfigRC.neotree-keys = nvim.dag.entryAnywhere ''
 
         -- Set up the actual keymaps
         local opts = { silent = true, noremap = true }
-        vim.api.nvim_set_keymap("n", "<leader>E", ":NeoTree<cr>", opts)
+        vim.api.nvim_set_keymap("n", "<leader>E", ":Neotree toggle<cr>", opts)
         vim.api.nvim_set_keymap("n", "<leader>eb", ":Neotree source=buffers position=left<cr>", opts)
         vim.api.nvim_set_keymap("n", "<leader>ef", ":Neotree filesystem reveal left<cr>", opts)
         vim.api.nvim_set_keymap("n", "<leader>eg", ":Neotree git_status<cr>", opts)
