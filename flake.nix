d1be172296588232a8659ef69d0e052cda07e15e
jason <jason@ridgway-taylor.co.uk>
2026-03-20 10:16:26 +0000

    chore: updated flake lock

--- flake.nix
+++ flake.nix
@@ -235,7 +235,7 @@
       flake = false;
     };
     null-ls = {
-      url = "github:jose-elias-alvarez/null-ls.nvim";
+      url = "github:nvimtools/none-ls.nvim";
       flake = false;
     };
     lsp-signature = {
